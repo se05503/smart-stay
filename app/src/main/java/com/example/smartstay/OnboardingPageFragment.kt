65a7cbcc591a4ac40885581194df6e596c65a2c4
se05503 <seyeongb18@gmail.com>
2025-05-19 22:49:28 +0900

    feat(ui): 선택지 번호에 따라 서로 다른 레이아웃 붙이기 - ViewStub 사용하기 (다음에는 addView도 시도해서 장단점 비교해보기) - 선택지 번호에 따라 서로 다른 뷰 inflate하기

--- app/src/main/java/com/example/smartstay/OnboardingPageFragment.kt
+++ app/src/main/java/com/example/smartstay/OnboardingPageFragment.kt
@@ -2,20 +2,22 @@ package com.example.smartstay
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.slider.Slider
 
-class OnboardingPageFragment: Fragment() {
+class OnboardingPageFragment : Fragment() {
 
     private var callback: OnboardingPageCallback? = null
+    private var progress: Int = 0
 
     companion object {
         fun newInstance(item: QuestionItem): OnboardingPageFragment {
@@ -41,7 +43,7 @@ class OnboardingPageFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_onboarding_page, container, false)
         val text = arguments?.getString("text")
-        val progress = arguments?.getInt("progress") ?: -1
+        progress = arguments?.getInt("progress") ?: -1
         view.findViewById<TextView>(R.id.tv_question).text = text
         view.findViewById<ProgressBar>(R.id.progressBar).setProgress(progress, true)
         return view
@@ -50,59 +52,127 @@ class OnboardingPageFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val viewStub = view.findViewById<ViewStub>(R.id.view_stub)
+        when(progress) {
+            1,4,5 -> {
+                // 이중 선택지
+                viewStub.layoutResource = R.layout.layout_initial_setting_dual_button
+                val inflatedView = viewStub.inflate()
+                when(progress) {
+                    1 -> {
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "여자"
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "남자"
+                    }
+                    4 -> {
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "기혼"
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "미혼"
+                    }
+                    5 -> {
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "있음"
+                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "없음"
+                    }
+                }
+            }
+            3 -> {
+                // 다중 선택지
+                viewStub.layoutResource = R.layout.layout_intial_setting_multiple_button
+                val inflatedView = viewStub.inflate()
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_1).text = "경영/관리직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_2).text = "기능/숙련공"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_3).text = "기술직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_4).text = "사무직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_5).text = "일반 작업직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_6).text = "자영업"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_7).text = "전문직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_8).text = "판매/\n서비스직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_9).text = "자유직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_10).text = "전업주부"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_11).text = "대학(원)생"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_12).text = "재수/\n입시/\n유학준비"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_13).text = "무직"
+                inflatedView.findViewById<TextView>(R.id.tv_multiple_option_14).text = "기타"
+                inflatedView.findViewById<MaterialCardView>(R.id.cv_question_fifteen).visibility = View.INVISIBLE
+            }
+            2, 6, 7 -> {
+                // 슬라이더
+                viewStub.layoutResource = R.layout.layout_initial_setting_slider
+                val inflatedView = viewStub.inflate()
+                when(progress) {
+                    2 -> {
+                        // 나이
+                        val sliderAge = inflatedView.findViewById<Slider>(R.id.slider)
+                        sliderAge.valueFrom = 1.0f
+                        sliderAge.valueTo = 100.0f
+                        sliderAge.value = 1.0f
+                        sliderAge.setLabelFormatter { value ->
+                            "${value.toInt()}살"
+                        }
+                        sliderAge.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
+                            override fun onStartTrackingTouch(slider: Slider) {
+
+                            }
+
+                            override fun onStopTrackingTouch(slider: Slider) {
+                                Toast.makeText(requireContext(), "value:${slider.value.toInt()}살", Toast.LENGTH_SHORT).show()
+                            }
+
+                        })
+                        inflatedView.findViewById<TextView>(R.id.tv_value_start).text = "1살"
+                        inflatedView.findViewById<TextView>(R.id.tv_value_end).text = "100살"
+                    }
+                    6 -> {
+                        // 가족 인원수
+                        val sliderFamilyCount = view.findViewById<Slider>(R.id.slider)
+                        sliderFamilyCount.valueFrom = 3.0f
+                        sliderFamilyCount.valueTo = 10.0f
+                        sliderFamilyCount.value = 3.0f
+                        sliderFamilyCount.setLabelFormatter { value ->
+                            "${value.toInt()}명"
+                        }
+                        sliderFamilyCount.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
+                            override fun onStartTrackingTouch(slider: Slider) {
+
+                            }
+
+                            override fun onStopTrackingTouch(slider: Slider) {
+                                Toast.makeText(requireContext(), "value:${slider.value.toInt()}명", Toast.LENGTH_SHORT).show()
+                            }
+
+                        })
+                        inflatedView.findViewById<TextView>(R.id.tv_value_start).text = "3명"
+                        inflatedView.findViewById<TextView>(R.id.tv_value_end).text = "10명"
+                    }
+                    7 -> {
+                        // 1인당 평균 월수익
+                        val sliderIncome = view.findViewById<Slider>(R.id.slider)
+                        sliderIncome.valueFrom = 0.0f
+                        sliderIncome.valueTo = 1000.0f
+                        sliderIncome.value = 0.0f
+                        sliderIncome.setLabelFormatter { value ->
+                            if(value.toInt()==1000) "${value.toInt()}만원~"
+                            else "${value.toInt()}만원"
+                        }
+                        sliderIncome.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
+                            override fun onStartTrackingTouch(slider: Slider) {
+
+                            }
+
+                            override fun onStopTrackingTouch(slider: Slider) {
+                                Toast.makeText(requireContext(), "value:${slider.value.toInt()}만원", Toast.LENGTH_SHORT).show()
+                            }
+
+                        })
+                        inflatedView.findViewById<TextView>(R.id.tv_value_start).text = "0만원"
+                        inflatedView.findViewById<TextView>(R.id.tv_value_end).text = "1,000만원~"
+                    }
+                }
+            }
+        }
+
         view.findViewById<AppCompatButton>(R.id.btn_next_page).setOnClickListener {
-            Log.d("ttest",""+callback)
             callback?.onNextPage()
         }
 
-        // 나이
-        val sliderAge = view.findViewById<Slider>(R.id.slider_age)
-        sliderAge.setLabelFormatter { value ->
-            "${value.toInt()}살"
-        }
-        sliderAge.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
-            override fun onStartTrackingTouch(slider: Slider) {
-
-            }
-
-            override fun onStopTrackingTouch(slider: Slider) {
-                Toast.makeText(requireContext(), "value:${slider.value.toInt()}살", Toast.LENGTH_SHORT).show()
-            }
-
-        })
-
-        // 가족 인원수
-        val sliderFamilyCount = view.findViewById<Slider>(R.id.slider_family_count)
-        sliderFamilyCount.setLabelFormatter { value ->
-            "${value.toInt()}명"
-        }
-        sliderFamilyCount.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
-            override fun onStartTrackingTouch(slider: Slider) {
-
-            }
-
-            override fun onStopTrackingTouch(slider: Slider) {
-                Toast.makeText(requireContext(), "value:${slider.value.toInt()}명", Toast.LENGTH_SHORT).show()
-            }
-
-        })
-
-        // 1인당 평균 월수익
-        val sliderIncome = view.findViewById<Slider>(R.id.slider_income)
-        sliderIncome.setLabelFormatter { value ->
-            if(value.toInt()==1000) "${value.toInt()}만원~"
-            else "${value.toInt()}만원"
-        }
-        sliderIncome.addOnSliderTouchListener(object: Slider.OnSliderTouchListener {
-            override fun onStartTrackingTouch(slider: Slider) {
-
-            }
-
-            override fun onStopTrackingTouch(slider: Slider) {
-                Toast.makeText(requireContext(), "value:${slider.value.toInt()}만원", Toast.LENGTH_SHORT).show()
-            }
-
-        })
     }
 
     override fun onDetach() {
