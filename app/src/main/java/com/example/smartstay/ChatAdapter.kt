e63ab8cfaaaee08e57502e3b99ed8aa70ec24ce3
se05503 <seyeongb18@gmail.com>
2025-05-28 14:04:59 +0900

    feat(chat): 채팅 어댑터 챗봇 뷰홀더 2개로 분리 - 사용자에게 메세지만 전달하는 단순한 UI를 가진 뷰홀더 1 - 숙소 추천해주는 복잡한 UI를 가진 뷰홀더 2 - 뷰타입 추가 생성 (VIEW_TYPE_CHATBOT_RECOMMEND, VIEW_TYPE_NONE)

--- app/src/main/java/com/example/smartstay/ChatAdapter.kt
+++ app/src/main/java/com/example/smartstay/ChatAdapter.kt
@@ -5,7 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.smartstay.databinding.ItemChatBotBinding
+import com.example.smartstay.databinding.ItemChatBotRecommendationBinding
+import com.example.smartstay.databinding.ItemChatBotTextBinding
 import com.example.smartstay.databinding.ItemChatUserBinding
 
 class ChatAdapter: ListAdapter<ChatModel, RecyclerView.ViewHolder>(differ) {
@@ -18,34 +19,68 @@ class ChatAdapter: ListAdapter<ChatModel, RecyclerView.ViewHolder>(differ) {
         }
     }
 
-    inner class ChatBotViewHolder(private val binding: ItemChatBotBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ChatBotTextViewHolder(private val binding: ItemChatBotTextBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ChatModel.ChatBotMessage) {
             binding.tvMessage.text = item.message
-            // TODO: 숙소 추천 view 띄우기
+        }
+    }
+
+    inner class ChatBotRecommendViewHolder(private val binding: ItemChatBotRecommendationBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ChatModel.ChatBotMessage) {
+            binding.tvMessage.text = item.message
+            val randomImages = listOf(
+                R.drawable.img_stay_1,
+                R.drawable.img_stay_2,
+                R.drawable.img_stay_3,
+                R.drawable.img_stay_4,
+                R.drawable.img_stay_5,
+                R.drawable.img_stay_6,
+                R.drawable.img_stay_7,
+                R.drawable.img_stay_8,
+                R.drawable.img_stay_9,
+                R.drawable.img_stay_10
+            )
+            binding.ivStayImage.setImageResource(randomImages.random())
+            binding.tvStayName.text = item.accommodationInfo?.name
+            binding.tvStayAddress.text = item.accommodationInfo?.address
+            binding.tvStayPrice.text = item.accommodationInfo?.pricePerNight.toString()
+//            binding.recyclerviewChatStayRecommendations.adapter = ChatRecommendationAdapter()
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return when(getItem(position)) {
-            is ChatModel.ChatBotMessage -> VIEW_TYPE_CHATBOT
-            is ChatModel.UserMessage -> VIEW_TYPE_USER
+        return when(val item = getItem(position)) {
+            is ChatModel.UserMessage -> {
+                VIEW_TYPE_USER
+            }
+            is ChatModel.ChatBotMessage -> {
+                when(item.type) {
+                    0 -> VIEW_TYPE_CHATBOT_TEXT
+                    1 -> VIEW_TYPE_CHATBOT_RECOMMEND
+                    else -> VIEW_TYPE_NONE
+                }
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if(viewType == VIEW_TYPE_CHATBOT) {
-            val binding = ItemChatBotBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            ChatBotViewHolder(binding)
-        } else {
+        return if(viewType == VIEW_TYPE_USER) {
             val binding = ItemChatUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             UserViewHolder(binding)
+        } else if(viewType == VIEW_TYPE_CHATBOT_TEXT) {
+            val binding = ItemChatBotTextBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ChatBotTextViewHolder(binding)
+        } else {
+            val binding = ItemChatBotRecommendationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ChatBotRecommendViewHolder(binding)
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when(holder) {
-            is ChatBotViewHolder -> holder.bind(getItem(position) as ChatModel.ChatBotMessage)
             is UserViewHolder -> holder.bind(getItem(position) as ChatModel.UserMessage)
+            is ChatBotTextViewHolder -> holder.bind(getItem(position) as ChatModel.ChatBotMessage)
+            is ChatBotRecommendViewHolder -> holder.bind(getItem(position) as ChatModel.ChatBotMessage)
         }
     }
 
@@ -67,7 +102,9 @@ class ChatAdapter: ListAdapter<ChatModel, RecyclerView.ViewHolder>(differ) {
 
         }
 
-        private const val VIEW_TYPE_CHATBOT = 0
-        private const val VIEW_TYPE_USER = 1
+        private const val VIEW_TYPE_USER = 0
+        private const val VIEW_TYPE_CHATBOT_TEXT = 1
+        private const val VIEW_TYPE_CHATBOT_RECOMMEND = 2
+        private const val VIEW_TYPE_NONE = 3 // 예외 처리용
     }
 }
