acd1deee8dfba131bb7059abc34daa9e025664a4
se05503 <seyeongb18@gmail.com>
2025-05-29 19:03:36 +0900

    refactor(onboarding): 코드 리팩토링 및 이중 선택지 기능 개선 - key 값 const val 로 분리 - viewBinding 사용 - 메소드 분리 - 1,4,5 페이지(이중 선택지)에 대해 한개만 선택가능하도록 개선

--- app/src/main/java/com/example/smartstay/OnboardingPageFragment.kt
+++ app/src/main/java/com/example/smartstay/OnboardingPageFragment.kt
@@ -2,21 +2,18 @@ package com.example.smartstay
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.ViewStub
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.smartstay.databinding.FragmentOnboardingPageBinding
 import com.google.android.material.card.MaterialCardView
-import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.slider.Slider
 
-class OnboardingPageFragment : Fragment() {
+class OnboardingPageFragment : Fragment(R.layout.fragment_onboarding_page) {
 
+    private lateinit var binding: FragmentOnboardingPageBinding
     private var callback: OnboardingPageCallback? = null
     private var progress: Int = 0
     private val viewModel: InitialSettingViewModel by activityViewModels()
@@ -24,13 +21,15 @@ class OnboardingPageFragment : Fragment() {
     companion object {
         fun newInstance(item: QuestionItem): OnboardingPageFragment {
             val fragment = OnboardingPageFragment()
-            val args = Bundle().apply {
-                putString("text", item.pageText)
-                putInt("progress", item.progress)
+            val arguments = Bundle().apply {
+                putString(KEY_QUESTION_TITLE, item.questionTitle)
+                putInt(KEY_QUESTION_PROGRESS, item.progress)
             }
-            fragment.arguments = args
+            fragment.arguments = arguments
             return fragment
         }
+        const val KEY_QUESTION_TITLE = "question_title"
+        const val KEY_QUESTION_PROGRESS = "question_progress"
     }
 
     override fun onAttach(context: Context) {
@@ -38,71 +37,93 @@ class OnboardingPageFragment : Fragment() {
         callback = parentFragment as? OnboardingPageCallback
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_onboarding_page, container, false)
-        val text = arguments?.getString("text")
-        progress = arguments?.getInt("progress") ?: -1
-        view.findViewById<TextView>(R.id.tv_question).text = text
-        view.findViewById<LinearProgressIndicator>(R.id.progressBar).setProgress(progress, true)
-        return view
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentOnboardingPageBinding.bind(view)
+        initViews()
 
-        val viewStub = view.findViewById<ViewStub>(R.id.view_stub)
+        val viewStub = binding.viewStub
+
         when(progress) {
             1,4,5 -> {
                 // 이중 선택지
                 viewStub.layoutResource = R.layout.layout_initial_setting_dual_button
                 val inflatedView = viewStub.inflate()
+                val tvOption1 = inflatedView.findViewById<TextView>(R.id.tv_dual_option_1)
+                val tvOption2 = inflatedView.findViewById<TextView>(R.id.tv_dual_option_2)
+                val cvOption1 = inflatedView.findViewById<MaterialCardView>(R.id.cv_option_1)
+                val cvOption2 = inflatedView.findViewById<MaterialCardView>(R.id.cv_option_2)
 
                 when(progress) {
                     1 -> {
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "여자"
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "남자"
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_1).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("F")
+
+                        tvOption1.text = getString(R.string.option_female)
+                        tvOption2.text = getString(R.string.option_male)
+
+                        cvOption1.setOnClickListener {
+                            if(!cvOption1.isSelected) {
+                                cvOption1.isSelected = true
+                                cvOption2.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.GENDER_FEMALE)
+                                viewModel.userInitialInfoList.remove(Constants.GENDER_MALE)
+                            }
                         }
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_2).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("M")
+
+                        cvOption2.setOnClickListener {
+                            if(!cvOption2.isSelected) {
+                                cvOption2.isSelected = true
+                                cvOption1.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.GENDER_MALE)
+                                viewModel.userInitialInfoList.remove(Constants.GENDER_FEMALE)
+                            }
                         }
                     }
+
                     4 -> {
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "기혼"
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "미혼"
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_1).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("Y")
+                        tvOption1.text = getString(R.string.option_married)
+                        tvOption2.text = getString(R.string.option_single)
+                        cvOption1.setOnClickListener {
+                            if(!cvOption1.isSelected) {
+                                cvOption1.isSelected = true
+                                cvOption2.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.ANSWER_YES)
+                                viewModel.userInitialInfoList.remove(Constants.ANSWER_NO)
+                            }
                         }
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_2).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("N")
+                        cvOption2.setOnClickListener {
+                            if(!cvOption2.isSelected) {
+                                cvOption2.isSelected = true
+                                cvOption1.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.ANSWER_NO)
+                                viewModel.userInitialInfoList.remove(Constants.ANSWER_YES)
+                            }
                         }
                     }
                     5 -> {
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_1).text = "있음"
-                        inflatedView.findViewById<TextView>(R.id.tv_dual_option_2).text = "없음"
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_1).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("Y")
+                        tvOption1.text = getString(R.string.option_present)
+                        tvOption2.text = getString(R.string.option_absent)
+                        cvOption1.setOnClickListener {
+                            if(!cvOption1.isSelected) {
+                                cvOption1.isSelected = true
+                                cvOption2.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.ANSWER_YES)
+                                viewModel.userInitialInfoList.remove(Constants.ANSWER_NO)
+                            }
                         }
-                        inflatedView.findViewById<MaterialCardView>(R.id.cv_option_2).setOnClickListener {
-                            it.isSelected = !it.isSelected
-                            if(it.isSelected == true) viewModel.userInitialInfoList.add("N")
+                        cvOption2.setOnClickListener {
+                            if(!cvOption2.isSelected) {
+                                cvOption2.isSelected = true
+                                cvOption1.isSelected = false
+                                viewModel.userInitialInfoList.add(Constants.ANSWER_NO)
+                                viewModel.userInitialInfoList.remove(Constants.ANSWER_YES)
+                            }
                         }
                     }
                 }
             }
             3 -> {
                 // 다중 선택지 - view
-                viewStub.layoutResource = R.layout.layout_intial_setting_multiple_button
+                viewStub.layoutResource = R.layout.layout_initial_setting_multiple_button
                 val inflatedView = viewStub.inflate()
                 inflatedView.findViewById<TextView>(R.id.tv_multiple_option_1).text = "경영/관리직"
                 inflatedView.findViewById<TextView>(R.id.tv_multiple_option_2).text = "기능/숙련공"
@@ -262,14 +283,25 @@ class OnboardingPageFragment : Fragment() {
             }
         }
 
-        view.findViewById<AppCompatButton>(R.id.btn_next_page).setOnClickListener {
+        initListeners()
+
+    }
+
+    private fun initListeners() = with(binding) {
+        btnNextPage.setOnClickListener {
             callback?.onNextPage()
         }
+    }
 
+    private fun initViews() = with(binding) {
+        tvQuestion.text = arguments?.getString(KEY_QUESTION_TITLE)
+        progress = arguments?.getInt(KEY_QUESTION_PROGRESS) ?: -1
+        progressBar.setProgress(progress, true) // refactoring
     }
 
     override fun onDetach() {
         super.onDetach()
         callback = null
     }
+
 }
