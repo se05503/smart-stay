ecf0a0aac148aa2327d3697d845f0d2c65cc9462
se05503 <seyeongb18@gmail.com>
2025-05-30 22:46:42 +0900

    test(map): 지도 마커 표시 테스트 - 데이터 1개 지도에 표시해보기 - marker, LatLng, CameraUpdate

--- app/src/main/java/com/example/smartstay/NaverMapActivity.kt
+++ app/src/main/java/com/example/smartstay/NaverMapActivity.kt
@@ -7,15 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.smartstay.databinding.ActivityNaverMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 
 class NaverMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityNaverMapBinding
-    private lateinit var mapView: MapView
-    private lateinit var naverMap: NaverMap
+    private lateinit var mapView: MapView // 지도에 대한 뷰 역할만 담당
+    private lateinit var naverMap: NaverMap // 실제 API 호출하는데 필요 + 인터페이스 역할을 하는 객체
     private var isMapInit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,6 +75,17 @@ class NaverMapActivity : AppCompatActivity(), OnMapReadyCallback {
         // NaverMap 객체가 준비되면 호출됨
         this.naverMap = naverMap
         isMapInit = true
+
+        // 마커 표시 테스트
+        // LatLng(위도, 경도) = LatLng(latitude, longitude)
+        val marker = Marker()
+        marker.position = LatLng(37.5665, 126.9780)
+        marker.captionText = "추천숙소"
+        marker.map = naverMap
+
+        // 카메라 이동
+        val cameraUpdate = CameraUpdate.scrollTo(marker.position)
+        naverMap.moveCamera(cameraUpdate)
     }
 
 
