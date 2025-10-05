283af04daec269ec87ca3d4f1ce419ec72dc6113
se05503 <seyeongb18@gmail.com>
2025-10-05 19:37:31 +0900

    [docs] TMAP - Vector Map API 속성 2차 정리 - 마커 설명 표시하기 - 선 그리기 - 자동차 경로안내 - 리버스 지오코딩

--- app/src/main/java/com/example/smartstay/TMapVectorFragment.kt
+++ app/src/main/java/com/example/smartstay/TMapVectorFragment.kt
@@ -1,6 +1,8 @@
 package com.example.smartstay
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.graphics.PointF
 import android.os.Bundle
 import android.view.View
@@ -12,6 +14,13 @@ import com.skt.tmap.TMapView
 import com.skt.tmap.overlay.TMapMarkerCluster
 import com.skt.tmap.overlay.TMapMarkerItem
 import com.skt.tmap.poi.TMapPOIItem
+import androidx.core.graphics.scale
+import com.skt.tmap.TMapData
+import com.skt.tmap.TMapInfo
+import com.skt.tmap.overlay.TMapPolyLine
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 
 class TMapVectorFragment : Fragment(R.layout.fragment_t_map_vector) {
 
@@ -33,7 +42,58 @@ class TMapVectorFragment : Fragment(R.layout.fragment_t_map_vector) {
         tmapView.setSKTMapApiKey(getString(R.string.sk_telecom_open_api_app_key))
         tmapView.setOnMapReadyListener(object: TMapView.OnMapReadyListener {
             override fun onMapReady() {
-                // TODO: 맵 로딩 완료 후 구현
+                /**
+                 * 자동차 경로안내
+                 * 처음 작성할 때 onMapReady 바깥에 둠 → 지도에 안보임
+                 * tmapView가 뷰에 attach된 직후 호출해야 한다.
+                 * 만약 아직 맵이 준비되기 전에 findPathData()를 호출하면 PolyLine이 화면에 안 뜰 수 있다.
+                 */
+                val startPoint = TMapPoint(37.570841, 126.985302) // 출발지 = SKT타워
+                val endPoint = TMapPoint(37.551135, 126.988205)
+                val tmapData = TMapData()
+                tmapData.findPathData(startPoint, endPoint, object: TMapData.OnFindPathDataListener {
+                    override fun onFindPathData(tmapPolyLine: TMapPolyLine?) {
+                        tmapPolyLine?.let {
+                            it.lineWidth = 3f
+                            it.lineColor = Color.BLUE
+                            it.lineAlpha = 255
+
+                            it.outLineWidth = 5f
+                            it.outLineColor = Color.RED
+                            it.outLineAlpha = 255
+
+                            tmapView.addTMapPolyLine(it)
+
+                            val info: TMapInfo = tmapView.getDisplayTMapInfo(it.linePointList)
+                            tmapView.zoomLevel = info.zoom
+                            tmapView.setCenterPoint(info.point.latitude, info.point.longitude)
+                        }
+                    }
+                })
+
+                /**
+                 * 리버스 지오코딩
+                 * 좌표(위도, 경도) → 주소로 변환
+                 * TMapData 객체의 convertGpsToAddress 메소드 사용
+                 */
+                val handler = Handler(Looper.getMainLooper()) { message ->
+                    if(message.what == 1) {
+                        val address = message.data.getString("address")
+                        Toast.makeText(context, address, Toast.LENGTH_SHORT).show()
+                    }
+                    false
+                }
+
+                TMapData().convertGpsToAddress(37.570841, 126.985302, object: TMapData.OnConvertGPSToAddressListener {
+                    override fun onConverGPSToAddress(address: String?) {
+                        val message = Message()
+                        val bundle = Bundle()
+                        bundle.putString("address", address)
+                        message.data = bundle
+                        message.what = 1
+                        handler.sendMessage(message)
+                    }
+                })
             }
         })
 
@@ -98,6 +158,43 @@ class TMapVectorFragment : Fragment(R.layout.fragment_t_map_vector) {
         markerCluster.clusteringDistance = 100.0
         markerCluster.clusterMaxSize = 100
         markerCluster.markers = clusterMarkerList
+
+        /**
+         * 마커 이동(Tracking) 하기라곤 나와있는데 마커 설명 표시하기 같음
+         * 실행했을 때 실제 이동하는 기능은 찾기 어려움
+         * 말풍선도 사용하려면 따로 커스터마이징 필요할 듯
+         * calloutTitle = 말풍선의 제목
+         * calloutSubTitle = 말풍선의 부제목
+         * canShowCallout = 마커 클릭 시 Callout(말풍선)을 표시할지 여부
+         * isAnimation = 마커가 지도에 추가될 때 부드럽게 나타나는 애니메이션을 적용할지 여부
+         * 마커 “이동” 애니메이션이 아니라, “마커가 처음 지도에 표시될 때” 살짝 튀어오르듯 나타나는 효과
+         */
+        val originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_sample_marker)
+        val resizedBitmap = originalBitmap.scale(150, 150, false)
+        val marker = TMapMarkerItem().apply {
+            id = "marker1"
+            icon = resizedBitmap
+            setTMapPoint(37.5665, 126.9780)
+            calloutTitle = "서울시청"
+            calloutSubTitle = "서울특별시 중구 세종대로 110"
+            canShowCallout = true
+            isAnimation = true
+        }
+        tmapView.addTMapMarkerItem(marker)
+
+        /**
+         * 선 그리기
+         * 대중교통/차 네비게이션 표시할 때 사용할 수 있어보임
+         * 단, 중심 좌표 설정은 따로 해야할 듯 → "자동차 경로안내" 에 잘 나와있음
+         */
+        val pointList = arrayListOf<TMapPoint>()
+        pointList.add(TMapPoint(37.472678, 126.920928))
+        pointList.add(TMapPoint(37.494473, 127.120742))
+        pointList.add(TMapPoint(37.405619, 127.091903))
+        val line = TMapPolyLine("line1", pointList)
+        tmapView.addTMapPolyLine(line)
+
+
     }
 
 }
