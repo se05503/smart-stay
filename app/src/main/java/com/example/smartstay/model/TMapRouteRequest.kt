064d551266b358e11842419ea9719e9784da5823
se05503 <seyeongb18@gmail.com>
2025-10-20 16:03:38 +0900

    refactor(map): 경로 탐색 DTO 수정 1. 필요 없는 응답 필드 임시 주석 처리 2. 누락 필드 추가 - 누락된 이유: API 명세서에 나와있는 메세지 예시랑 실제 API 테스트에 나와있는 응답 예시가 다름 3. 요청 필드에 count 추가 - 경로 1개만 받아보고 UI 먼저 그린 이후에 카운트 갯수 늘리기

--- app/src/main/java/com/example/smartstay/model/TMapRouteRequest.kt
+++ app/src/main/java/com/example/smartstay/model/TMapRouteRequest.kt
@@ -4,7 +4,8 @@ data class TMapRouteRequest(
     val startX: String,
     val startY: String,
     val endX: String,
-    val endY: String
+    val endY: String,
+    val count: Int
 )
 
 data class TMapRouteResponse(
@@ -18,35 +19,35 @@ data class MetaData(
 
 data class RequestParameters(
     val busCount: Int? = null, // 버스 결과 개수
-    val expressbusCount: Int? = null, // 고속/시외 버스 결과 개수
     val subwayCount: Int? = null, // 지하철 결과 개수
-    val airplaneCount: Int? = null, // 항공 결과 개수
-    val wideareaRouteCount: Int? = null, // 광역 노선 결과 개수
-    val subwayBusCount: Int? = null, // 지하철 + 버스 결과 개수
-    val ferryCount: Int? = null, // 해운 결과 개수
-    val trainCount: Int? = null, // 기차 결과 개수
+//    val expressbusCount: Int? = null, // 고속/시외 버스 결과 개수
+//    val airplaneCount: Int? = null, // 항공 결과 개수
+//    val wideareaRouteCount: Int? = null, // 광역 노선 결과 개수
+//    val subwayBusCount: Int? = null, // 지하철 + 버스 결과 개수
+//    val ferryCount: Int? = null, // 해운 결과 개수
+//    val trainCount: Int? = null, // 기차 결과 개수
 
-    val locale: String? = null, // 언어코드(ko, en)
+//    val locale: String? = null, // 언어코드(ko, en)
     val endY: String, // 도착지 좌표(위도)
     val endX: String, // 도착지 좌표(경도)
     val startY: String, // 출발지 좌표(위도)
     val startX: String, // 출발지 좌표(경도)
-    val reqDttm: String // 요청 일시 (YYYYMMDDhhmmss)
+//    val reqDttm: String // 요청 일시 (YYYYMMDDhhmmss)
 )
 
 data class Plan(
-    val itineraries: List<Itinerary>
+    val itineraries: List<Itinerary> // 데이터 상세정보 최상위 노드 ★ 얘를 한개만 가져오는게 나을 것 같다.
 )
 
 data class Itinerary(
     val fare: Fare, // 금액 최상위 노드
     val totalTime: Int, // 총 소요 시간(sec)
+    val legs: List<Leg>? = null, // 대중교통 정보 최상위 노드
     val totalWalkTime: Int? = null, // 총 보행자 소요시간(sec)
-    val pathType: Int? = null, // 경로 탐색 결과 종류 (1-지하철, 2-버스, 3-버스+지하철, 4-고속/시외버스, 5-기차, 6-항공, 7-해운)
     val transferCount: Int? = null, // 환승 횟수
     val totalDistance: Int? = null, // 총 이동거리(m)
-    val totalWalkDistance: Int? = null, // 총 보행자 이동 거리(m)
-    val legs: List<Leg>? = null // 대중교옽 정보 최상위 노드
+    val pathType: Int? = null, // (★) 경로 탐색 결과 종류 (1-지하철, 2-버스, 3-버스+지하철, 4-고속/시외버스, 5-기차, 6-항공, 7-해운)
+    val totalWalkDistance: Int? = null // 총 보행자 이동 거리(m)
 )
 
 data class Fare(
@@ -59,18 +60,41 @@ data class Regular(
 )
 
 data class Currency(
-    val symbol: String, // 금액 상징(￦)
+//    val symbol: String, // 금액 상징(￦)
     val currency: String, // 금액 단위(원)
-    val currencyCode: String // 금액 단위 코드(KRW)
+//    val currencyCode: String // 금액 단위 코드(KRW)
 )
 
 data class Leg(
     val mode: String, // 이동 수단(WALK, BUS, SUBWAY, EXPRESS BUS, TRAIN, AIRPLANE, FERRY)
+    val routeColor: String? = null, // [대중교통] 노선 색상 (도보 경로 시 null 값 리턴)
     val sectionTime: Int, // 구간별 소요 시간 (sec)
+    val route: String? = null, // [대중교통] 노선 명칭 ex) 수도권3호선
+    val routeId: String? = null, // [대중교통] 노선 ID
     val distance: Int, // 구간별 이동 거리 (m)
+    val service: Int? = null, // [대중교통] 이동수단 운행 여부 flag → 1: 운행중 / 0: 운행종료
     val start: Location, // 시작 위치
+    val passStopList: PassStopList, // [대중교통] 구간 정류장 정보
     val end: Location, // 도착 위치
-    val steps: List<Step> // 도보 이동 시 상세 경로
+    val type: Int? = null, // [대중교통] 이동수단별 노선코드
+    val passShape: PassShape? = null, // [대중교통] 구간 좌표 최상위 노드
+    val steps: List<Step> // [도보] 구간별 도보상세정보 최상위 노드
+)
+
+data class PassShape(
+    val linestring: String // [대중교통] 구간 좌표(WGS84)
+)
+
+data class PassStopList(
+    val stationList: List<StationList> // 정류장 상세정보
+)
+
+data class StationList(
+    val index: Int, // 순번
+    val stationName: String, // 정류장 명칭
+    val lon: String, // 정류장 좌표(경도 - WGS84)
+    val lat: String, // 정류장 좌표(위도 - WGS84)
+    val stationID: String // 정류장 ID. KORAIL ID는 Station ID의 끝 4자리를 사용합니다. Station ID : 500036 → KORAIL ID : 0036(광주송정역)
 )
 
 data class Location(
@@ -83,5 +107,5 @@ data class Step(
     val streetName: String, // 도로명
     val distance: Int, // 도보 이동거리(m)
     val description: String, // 도로 구간 정보
-    val linestring: String // 도보 구간 좌표
+    val linestring: String // 도보 구간 좌표 (WGS84)
 )
