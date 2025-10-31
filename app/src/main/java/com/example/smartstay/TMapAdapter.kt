2d765a061fd31810f5be19a1f8ac9521db166428
se05503 <seyeongb18@gmail.com>
2025-10-31 12:51:22 +0900

    refactor(map): 첫번째 숙소 이미지만 가져오기

--- app/src/main/java/com/example/smartstay/TMapAdapter.kt
+++ app/src/main/java/com/example/smartstay/TMapAdapter.kt
@@ -2,15 +2,28 @@ package com.example.smartstay
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.smartstay.databinding.ItemTmapAccomodationBinding
-import com.example.smartstay.model.AccommodationInfo
+import com.example.smartstay.model.accommodation.Destination
 
-class TMapAdapter(private val onClicked: (AccommodationInfo) -> Unit): ListAdapter<AccommodationInfo, TMapAdapter.TMapViewHolder>(differ) {
+class TMapAdapter(private val onClicked: (Destination) -> Unit): ListAdapter<Destination, TMapAdapter.TMapViewHolder>(differ) {
+
+    inner class TMapViewHolder(private val binding: ItemTmapAccomodationBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Destination) = with(binding) {
+            val accommodation = item.accommodation
+            Glide.with(ivAccommodationPhoto).load(accommodation.image.first()).into(ivAccommodationPhoto)
+            tvTMapAccommodationType.text = accommodation.type
+            tvTMapAccommodationName.text = accommodation.name
+            tvTMapAccommodationAddress.text = accommodation.address
+            tvTMapAccommodationMinimumPrice.text = "${Utils.formatPrice(accommodation.minimumPrice)}~"
+            root.setOnClickListener {
+                onClicked(item)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,31 +40,18 @@ class TMapAdapter(private val onClicked: (AccommodationInfo) -> Unit): ListAdapt
         holder.bind(currentList[position])
     }
 
-    inner class TMapViewHolder(private val binding: ItemTmapAccomodationBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: AccommodationInfo) = with(binding) {
-            Glide.with(ivAccommodationPhoto).load(item.image).into(ivAccommodationPhoto)
-            tvTMapAccommodationType.text = item.type
-            tvTMapAccommodationName.text = item.name
-            tvTMapAccommodationAddress.text = item.address
-            tvTMapAccommodationMinimumPrice.text = "${Utils.formatPrice(item.minimumPrice)}~"
-            root.setOnClickListener {
-                onClicked(item)
-            }
-        }
-    }
-
     companion object {
-        val differ = object : DiffUtil.ItemCallback<AccommodationInfo>() {
+        val differ = object : DiffUtil.ItemCallback<Destination>() {
             override fun areItemsTheSame(
-                oldItem: AccommodationInfo,
-                newItem: AccommodationInfo
+                oldItem: Destination,
+                newItem: Destination
             ): Boolean {
                 return oldItem === newItem
             }
 
             override fun areContentsTheSame(
-                oldItem: AccommodationInfo,
-                newItem: AccommodationInfo
+                oldItem: Destination,
+                newItem: Destination
             ): Boolean {
                 return oldItem == newItem
             }
