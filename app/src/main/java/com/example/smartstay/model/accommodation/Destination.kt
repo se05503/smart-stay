cba380c9a83990462cd3b41301237e4c1e9cb613
se05503 <seyeongb18@gmail.com>
2025-11-11 16:13:34 +0900

    chore(map): 관광지 → 추정 방문객수, 혼잡도 유무 변수 제거 - 팀원 회의 피드백 반영

--- app/src/main/java/com/example/smartstay/model/accommodation/Destination.kt
+++ app/src/main/java/com/example/smartstay/model/accommodation/Destination.kt
@@ -41,7 +41,5 @@ data class Attraction(
     val category: String, // 장소 분류 → 쇼핑(shopping), 레저/스포츠(sports), 관광명소(tour)
     val lat: Double,
     val lng: Double,
-    val congestionYn: String, // 장소 혼잡도 유무 → Y, N
-    val count: Int, // 추정 방문 여행객수
     val distance: Double // 숙소로부터의 거리
 ): Parcelable
